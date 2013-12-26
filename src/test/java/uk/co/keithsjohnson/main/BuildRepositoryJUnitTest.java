807abdc7921a2b777ff6b537905ca9fdf8325be5
keithsjohnson <keithsjohnson@yahoo.com>
2013-12-26 23:25:50 +0000

    Update unit test

--- src/test/java/uk/co/keithsjohnson/main/BuildRepositoryJUnitTest.java
+++ src/test/java/uk/co/keithsjohnson/main/BuildRepositoryJUnitTest.java
@@ -2,6 +2,8 @@ package uk.co.keithsjohnson.main;
 
 import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -11,44 +13,89 @@ import uk.co.keithsjohnson.repo.BuildRepository;
 
 public class BuildRepositoryJUnitTest {
 
+	private BuildRepository testSubject;
+
+	private AbstractApplicationContext context;
+
+	@Before
+	public void setup() {
+		context = new ClassPathXmlApplicationContext("application-context.xml");
+		testSubject = context.getBean(BuildRepository.class);
+	}
+
+	@After
+	public void teardown() {
+		context.close();
+	}
+
 	@Test
 	public void shouldBuildRepository() {
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
-				"application-context.xml");
-		BuildRepository repository = context.getBean(BuildRepository.class);
 
-		// save a couple of Builds
-		repository.save(new Build("Jack"));
-		repository.save(new Build("Chloe"));
-		repository.save(new Build("Kim"));
-		repository.save(new Build("David"));
-		repository.save(new Build("Michelle"));
+		// save a 5 of Builds
+		testSubject.save(new Build("Jack"));
+		testSubject.save(new Build("Chloe"));
+		testSubject.save(new Build("Kim"));
+		testSubject.save(new Build("David"));
+		testSubject.save(new Build("Michelle"));
 
 		// fetch all Builds
-		Iterable<Build> Builds = repository.findAll();
+		Iterable<Build> builds = testSubject.findAll();
 		System.out.println("Builds found with findAll():");
 		System.out.println("-------------------------------");
-		for (Build Build : Builds) {
-			System.out.println(Build);
+		for (Build build : builds) {
+			System.out.println(build);
 		}
 		System.out.println();
 
 		// fetch an individual Build by ID
-		Build Build = repository.findOne(1L);
+		Build build = testSubject.findOne(1L);
 		System.out.println("Build found with findOne(1L):");
 		System.out.println("--------------------------------");
-		System.out.println(Build);
+		System.out.println(build);
 		System.out.println();
 
 		// fetch Builds by name
-		List<Build> jacks = repository.findByName("Jack");
+		List<Build> names = testSubject.findByName("Jack");
 		System.out.println("Build found with findByName('Jack'):");
 		System.out.println("--------------------------------------------");
-		for (Build jack : jacks) {
-			System.out.println(jack);
+		for (Build name : names) {
+			System.out.println(name);
 		}
+	}
 
-		context.close();
+	@Test
+	public void shouldBuildRepository2() {
+
+		// save a 5 of Builds
+		testSubject.save(new Build("Jack"));
+		testSubject.save(new Build("Chloe"));
+		testSubject.save(new Build("Kim"));
+		testSubject.save(new Build("David"));
+		testSubject.save(new Build("Michelle"));
+
+		// fetch all Builds
+		Iterable<Build> builds = testSubject.findAll();
+		System.out.println("Builds found with findAll():");
+		System.out.println("-------------------------------");
+		for (Build build : builds) {
+			System.out.println(build);
+		}
+		System.out.println();
+
+		// fetch an individual Build by ID
+		Build build = testSubject.findOne(2L);
+		System.out.println("Build found with findOne(2L):");
+		System.out.println("--------------------------------");
+		System.out.println(build);
+		System.out.println();
+
+		// fetch Builds by name
+		List<Build> names = testSubject.findByName("Kim");
+		System.out.println("Build found with findByName('Kim'):");
+		System.out.println("--------------------------------------------");
+		for (Build name : names) {
+			System.out.println(name);
+		}
 	}
 
 }
